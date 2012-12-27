893cf33cbb57b9574c2d68b9778982a52a01c401
James Cryer <info@jamescryer.com>
2012-12-27 17:28:42 +0000

    Adding start of test suite

---
+++ test/runner_test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+suite('Runner', function() {
+  test('Runner should detect if PHPUnit is installed', function(done) {
+    assert.equal(true, true);
+
+    done();
+  });
+});
