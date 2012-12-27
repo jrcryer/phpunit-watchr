777e71ae4a93b517dc415abbb23195935e32ab3c
James Cryer <info@jamescryer.com>
2012-12-27 23:43:38 +0000

    Adding tests

--- test/runner_test.js
+++ test/runner_test.js
@@ -1,9 +1,49 @@
 var assert = require('assert');
+var sinon  = require('sinon');
+var watchr = require('watchr');
 
-suite('Runner', function() {
-  test('Runner should detect if PHPUnit is installed', function(done) {
-    assert.equal(true, true);
+describe('Runner', function() {
 
-    done();
-  });
+	var runner = require('../lib/runner');
+
+	it('should detect if PHPUnit is installed', function(done) {
+		var sys      = sinon.mock().never();
+		var watcher  = sinon.mock(watchr).expects('watch').once();
+		var exec     = sinon.stub().callsArgWith(1, {}, 'PHPUnit');
+		var sut      = new runner(sys, watchr, exec);
+
+		sut.watch('/path');
+		assert.ok(exec.calledOnce);
+		assert.equal('phpunit --version', exec.getCall(0).args[0]);
+		assert.ok(watcher.calledOnce);
+		watchr.watch.restore();
+		done();
+	});
+
+	it('should not watch directory if PHPUnit is not available', function(done) {
+		var sys      = sinon.mock().never();
+		var watcher  = sinon.mock(watchr).expects('watch').never();
+		var response = sinon.stub('NOT', 'match').withArgs(/^PHPUnit/).returns(false);
+		var exec     = sinon.stub().withArgs('phpunit --version').returns({}, response);
+		var sut = new runner(sys, watchr, exec);
+
+		sut.watch('/path');
+		assert.ok(exec.calledOnce);
+		assert.equal('phpunit --version', exec.getCall(0).args[0]);
+		watchr.watch.restore();
+		done();
+	});
+
+	it('should default configuration to colors', function(done) {
+		var sut = new runner({}, {}, {});
+		assert.equal(sut.getSettings(), '--colors');
+		done();
+	});
+
+	it('can set configuration for PHPUnit', function(done) {
+		var sut = new runner({}, {}, {});
+		sut.setup('--strict');
+		assert.equal(sut.getSettings(), '--strict');
+		done();
+	});
 });
